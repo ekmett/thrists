cae0d854fc762ab0f2108eeb27568b9aaa2c8c2e
Edward Kmett <ekmett@gmail.com>
2014-07-02 00:25:03 -0400

    added naive uncons

--- wip/Mini.hs
+++ wip/Mini.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE GADTs, BangPatterns #-}
 {-# OPTIONS_GHC -Wall #-}
 
+import Control.Category.Free.View
+
 data P r a c where
   P :: r b c -> r a b -> P r a c
 
@@ -17,6 +19,10 @@ data R l m a c where
   R :: !(R l (P m) b c) -> m a b -> R l m a c
   NR :: R r r a a
 
+-- regularity:
+-- * 0s and 2s alternate.
+-- * Q maximally pushes runs of 1s into the L and R parameters
+-- *
 data Q r a b where
   Q0 :: Q r a a
   Q1 :: r a b -> Q r a b
@@ -40,6 +46,16 @@ dcons p a (L b l) m (R r c) s = Q p NL (Q (D2 a b) l m r (D1 c)) NR s
 dcons _ _ NL _ R{} _ = error "impossible"
 dcons _ _ L{} _ NR _ = error "impossible"
 dcons _ _ NL (Q D2{} _ _ _ _) _ _ = error "non-regular"
+
+-- extract from a non-0-exposed queue
+nuncons :: Q r a c -> View r (Q r) a c
+nuncons Q0 = Empty
+nuncons (Q1 a) = a :| Q0
+nuncons (Q (D1 a) NL Q0 NR (D1 b))   = a :| Q1 b
+nuncons (Q (D1 a) NL Q0 NR (D2 b c)) = a :| Q (D1 b) NL Q0 NR (D1 c)
+nuncons (Q (D1 a) l m r s)           = a :| Q D0 l m r s
+nuncons (Q (D2 a b) l m r s)         = a :| Q (D1 b) l m r s
+nuncons (Q D0 _ _ _ _) = error "non-regular"
 
 {-
 deep :: D a -> Q (P a) -> D a -> Q a
